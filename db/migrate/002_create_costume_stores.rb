1a0d8a68ac32d39bee2ea5b5c6a1f28bfbe41801
Teerth Patel <teerth1205@gmail.com>
2020-02-26 23:46:40 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,14 @@
 # Create your costume_stores migration here
+
+class CreateCostumes < ActiveRecord::Migration[4.2]
+  def change
+    create_table :costume_stores do |t|
+      t.string :name 
+      t.string  :location
+      t.string :costume_inventory
+      t.integer :number_of_employees
+      t.boolean :in_business 
+      t.integer :opening_time
+      t.integer :closing_time 
+    end 
+  end 
