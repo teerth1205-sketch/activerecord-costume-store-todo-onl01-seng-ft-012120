6c30d67a01beb2d1be972693665706c25cf2128d
Teerth Patel <teerth1205@gmail.com>
2020-02-26 23:56:35 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -5,11 +5,11 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
     create_table :costume_stores do |t|
       t.string :name 
       t.string  :location
-      t.string :costume_inventory
-      t.integer :number_of_employees
-      t.boolean :in_business 
-      t.integer :opening_time
-      t.integer :closing_time 
+      t.integer :costume_inventory
+      t.integer :num_of_employees
+      t.boolean :still_in_business 
+      t.datetime :opening_time
+      t.datetime :closing_time 
     end 
   end 
   end 
