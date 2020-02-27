da677db0c320bffa23586c2d2c492fb1522cdb64
Teerth Patel <teerth1205@gmail.com>
2020-02-27 00:01:46 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -9,7 +9,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
       t.boolean :family_friendly
       t.datetime :opening_date
       t.datetime :closing_date 
-      t.
+      t.text :long_description
     end 
   end 
   end 
