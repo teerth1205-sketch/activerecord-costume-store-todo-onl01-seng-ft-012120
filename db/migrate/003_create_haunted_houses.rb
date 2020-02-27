e86709111dee4634af5deae5359c530d261be7ad
Teerth Patel <teerth1205@gmail.com>
2020-02-27 00:00:48 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -4,11 +4,12 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
     create_table :costume_stores do |t|
       t.string :name 
       t.string  :location
-      t.integer :costume_inventory
-      t.integer :num_of_employees
-      t.boolean :still_in_business 
-      t.datetime :opening_time
-      t.datetime :closing_time 
+      t.string  :theme
+      t.integer :price
+      t.boolean :family_friendly
+      t.datetime :opening_date
+      t.datetime :closing_date 
+      t.
     end 
   end 
   end 
