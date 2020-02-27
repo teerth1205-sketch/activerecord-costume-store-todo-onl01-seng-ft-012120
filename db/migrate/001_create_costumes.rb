ab4adb096415b5db37fd014a3778b545704899e7
Teerth Patel <teerth1205@gmail.com>
2020-02-27 00:14:16 +0000

    Done.

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -1,5 +1,17 @@
 # Create a class and inherit from ActiveRecord::Migration
 
+class CreateCostumes < ActiveRecord::Migration[4.2]
+  def change
+    create_table :costumes do |t|
+      t.string :name 
+      t.integer :price
+      t.string :image_url
+      t.integer :size
+      t.timestamps
+    end 
+  end 
+  
+end
 # By convention, the class name should match the part of the
 # file name after the number, so in this case:
 # 002_create_costumes.rb becomes class CreateCostumes
